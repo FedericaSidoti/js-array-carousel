3e2813cd0840eb48dfee732f80981ec5e3c5f324
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-10 18:38:52 +0200

    ADD- event listener

--- js/app.js
+++ js/app.js
@@ -6,24 +6,37 @@ const arrayImages = [
     "./img/04.webp",
     "./img/05.webp"
 ]
-// - PER OGNI immagine, creare un div che la contenga
+// - PER OGNI immagine, creare un div che la contenga e inserirlo nell'html
 const imgContainerDomElement = document.querySelector('.img-container');
-console.log (imgContainerDomElement)
+//console.log (imgContainerDomElement)
 
 for(let i = 0; i < arrayImages.length; i++) {
     const currentIMG = arrayImages[i];
     //console.log(currentIMG)
     const htmlString = `
-    <div>
+    <div class="img">
         <img src="${currentIMG}">
     </div>
     `
-    console.log(htmlString)
+    //console.log(htmlString)
     imgContainerDomElement.innerHTML += htmlString
 }
 
+const imgElements = document.querySelectorAll('.img')
+//console.log(imgElements)
+let currentIMG = imgElements[0]
+currentIMG.classList.add('active')
+let classActive = true ;
 
+//aggiungere alle frecce un eventlistener
+btndownDOMElement = document.getElementById('btn-down')
+btndownDOMElement.addEventListener ('click', function (){
+    
+    
+})    
 
+/*
 
+}*/
+// AL CLICK la classe active viene aggiunta all'immagine successiva a quella mostrata. 
 
-// - inserire ogni div creato nell'HTML
